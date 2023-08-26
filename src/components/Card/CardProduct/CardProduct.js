bdb165aa138296f5d3ef360b01ab12315f72984f
tminhbao <tminhbao2001@gmail.com>
2023-08-26 20:58:22 +0700

    render all products

--- src/components/Card/CardProduct/CardProduct.js
+++ src/components/Card/CardProduct/CardProduct.js
@@ -1,8 +1,10 @@
 import React from 'react'
 import logo from '../../../assets/nike.png'
 import ProductItem from '../../Product/ProductItem/ProductItem'
+import shoes from "../../../data/shoes.json"
 
 const CardProduct = () => {
+    console.log(shoes.shoes)
     return (
         <div className='card row overflow-auto mx-auto shadow p-3 mb-5 bg-body'>
             <div class="card-body p-0 position-relative">
@@ -13,12 +15,7 @@ const CardProduct = () => {
                     <h3 class="card-title fw-bold">Our products</h3>
                 </div>
                 <div className='card-content'>
-                    <ProductItem />
-                    <ProductItem />
-                    <ProductItem />
-                    <ProductItem />
-                    <ProductItem />
-                    <ProductItem />
+                    {shoes.shoes.map(shoe => <ProductItem shoe={shoe} key={shoe?.id} />)}
                 </div>
             </div>
         </div>
